4a914864708c7100eff4addae306b6eb7146efe8
Evgeniya.S <reg.subbotina@gmail.com>
2023-05-07 13:37:10 +0300

    added Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string NumbersFromNTo1 (int N)
+{
+    if (N > 0) return $"{N} " + NumbersFromNTo1 (N - 1);
+    else return String.Empty;
+}
+
+System.Console.WriteLine(NumbersFromNTo1 (10));
